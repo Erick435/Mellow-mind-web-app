49aacbffea7942dc52d02619a3ad3697d0a95298
davidd0930 <118568165+davidd0930@users.noreply.github.com>
2023-09-04 12:13:53 -0400

    some changes

--- src/components/googleSignin/SignIn.jsx
+++ src/components/googleSignin/SignIn.jsx
@@ -1,84 +1,70 @@
-import React from "react";
-import { auth, provider } from "./config";
+import React, { useState, useEffect } from "react";
 import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "./config";
+import { FaGoogle, FaStopwatch, FaSpinner, FaMusic, FaBullseye } from 'react-icons/fa6';
 import './signin.css';
-import { FaGoogle, FaStopwatch, FaSpinner, FaMusic, FaBullseye } from 'react-icons/fa6'
 
-// ------------------------ SIGNING IN FUNCTION/ GOOGLE FIREBASE ---------------
-function SignIn({ handleLogin }) {
+const Carousel = ({ images, currentImageIndex, setCurrentImageIndex }) => (
+    <div className="carousel-container">
+        <img src={images[currentImageIndex]} alt="Carousel content" />
+        <div className="carousel-dots">
+            {images.map((_, index) => (
+                <button
+                    key={index}
+                    className={`dot ${currentImageIndex === index ? 'active' : ''}`}
+                    onClick={() => setCurrentImageIndex(index)}
+                ></button>
+            ))}
+        </div>
+    </div>
+);
+
+const Navbar = ({ signInWithGoogle }) => (
+    <div className="navbar">
+        <div className="title-container">
+            <img src="/logo-trsprnt.png" alt="Mellow Mind Logo" />
+            <h1>MellowMind</h1>
+        </div>
+        <div className="signin-link" onClick={signInWithGoogle}>
+            <b>Sign In with
+                <div className="signIn-button-google">
+                    <FaGoogle className="google-G" />
+                    oogle
+                </div>
+            </b>
+        </div>
+    </div>
+);
+
+const Footer = ({ link }) => (
+    <div className="footer">
+        © 2023 i4software beta 2.0 application. All rights reserved. <br />
+        Interested in giving feedback? <a href={link} target="_blank" rel="noopener noreferrer">Click here</a>
+    </div>
+);
+
+
+const SignIn = ({ handleLogin }) => {
+    const images = ["/ss1.png", "/ss2.png", "/ss3.png"];
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentImageIndex(prevIndex => (prevIndex + 1) % images.length);
+        }, 5000);
+        return () => clearInterval(timer);
+    }, [images.length]);
+
     const signInWithGoogle = () => {
         signInWithPopup(auth, provider)
-            .then((data) => {
-                handleLogin(data.user);
-            })
-            .catch((error) => {
-                console.error("Error during sign-in:", error);
-            });
+            .then(data => handleLogin(data.user))
+            .catch(error => console.error("Error during sign-in:", error));
     };
-    // ============================================================================
-
-
-    // ------------------------ ADDING IMAGES TO THE CAROUSEL LANDING PAGE -----------
-
-    const images = [
-        "/ss1.png",
-        "/ss2.png",
-        "/ss3.png",
-    ]
-
-    const [currentImageIndex, setCurrentImageIndex] = React.useState(0);
-
-    // useEffect to handle transition 
-    React.useEffect(() => {
-        const timer = setInterval(() => {
-            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 5000); // change images every 5 seconds
-
-        // Cleanup: clear the timer when the component is unmounted or before re-running the effect
-        return () => clearInterval(timer);
-    }, [currentImageIndex]);
-
-
-    // ===============================================================================
-
 
     return (
         <div className="landingPageContainer">
-
-            {/*                          NAVBAR                         */}
-            <div className="navbar">
-                <div className="title-container">
-                    <img src="/logo-trsprnt.png" alt="Mellow Mind Logo" />
-                    <h1>MellowMind</h1>
-                </div>
-                <div className="signin-link" onClick={signInWithGoogle}>
-                    <b> Sign In with
-                        <div className="signIn-button-google">
-                            <FaGoogle className="google-G" />
-                            oogle
-                        </div>
-                    </b>
-                </div>
-            </div>
-            {/* ========================================================= */}
-
-            {/*                         CAROUSEL                        */}
-
-            <div className="carousel-container">
-                <img src={images[currentImageIndex]} alt="Carousel content" />
-
-                <div className="carousel-dots">
-                    {images.map((_, index) => (
-                        <button
-                            key={index}
-                            className={`dot ${currentImageIndex === index ? 'active' : ''}`}
-                            onClick={() => setCurrentImageIndex(index)}
-                        ></button>
-                    ))}
-                </div>
-
-            </div>
-            {/* ================================================================== */}
+            <Navbar signInWithGoogle={signInWithGoogle} />
+            <Carousel images={images} currentImageIndex={currentImageIndex} setCurrentImageIndex={setCurrentImageIndex} />
             <div className="infos">
                 <h1 className="instructions">
                     <FaMusic />  Listen <br />
@@ -87,6 +73,7 @@ function SignIn({ handleLogin }) {
                     <FaBullseye /> Experience
                 </h1>
             </div>
+            <Footer link="https://forms.gle/YP93waRthLBaf6Jg8" />
         </div>
     );
 }
