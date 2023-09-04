41f01ba0f28a3c74a284c05cd88394d221d53115
davidd0930 <118568165+davidd0930@users.noreply.github.com>
2023-09-04 01:12:58 -0400

    footer changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,26 @@
-// Footer.js
-import React from 'react';
+import React, { useState } from 'react';
 
 function Footer() {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div style={{ position: 'relative', left: '22px', bottom: '70px', width: '100%', padding: '10px 20px', textAlign: 'center' }}>
-      <a href="https://forms.gle/YP93waRthLBaf6Jg8" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'white' }}>
-        Give us your feedback!
-      </a>
+    <div className="App" style={{ position: 'relative', zIndex: 0, marginBottom: '35px' }}>
+      <div style={{ position: 'relative', bottom: '70px', width: '100%', padding: '10px 20px', textAlign: 'center' }}>
+        <a 
+          href="https://forms.gle/YP93waRthLBaf6Jg8" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          style={{
+            textDecoration: 'none', 
+            color: isHovered ? 'black' : 'white',
+            fontSize: '20px'  // adjust this value as per your requirement
+          }}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          Give us your feedback!
+        </a>
+      </div>
     </div>
   );
 }
